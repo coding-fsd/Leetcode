fa521e46690a0a788ede51a940572865f2144e3d
ShardulsGit <workshardul@gmail.com>
2026-02-19 12:56:38 -0800

    Time: 1761 ms (5.03%) | Memory: 26.4 MB (75.02%) - LeetSync

---
+++ 189-rotate-array/rotate-array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        while k != 0 :
+            t = nums.pop()
+            nums.insert(0,t)
+            k-=1
